9325191a440ff93030de6e6be55fcfc2c6a4b007
Abiexperiments <abigailgarcia900200@gmail.com>
2024-11-21 16:18:31 -0600

    Add files via upload

---
+++ Ejemplo5_1.java
@@ -0,0 +1,15 @@
+package U5Ejemplos;
+
+public class Ejemplo5_1 {
+
+	public static void main(String[] args) {
+int num=division();
+System.out.println(num);
+	}
+	
+public static int division() {
+	return 3/0;
+}
+	
+
+}
